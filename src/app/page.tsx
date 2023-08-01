5fa17ad17cb3b64ad4910f39fb100ee6cb6dd2cd
Philipp Drebes <philipp.drebes@gmail.com>
2023-08-01 14:16:15 +0200

    change welcome text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,38 +5,38 @@ const WelcomeText = () => {
         Mountainbike & Politik: Dein Wahlleitfaden für den Kanton Zug
       </h1>
       <p className="mb-4">
-        Herzlich willkommen auf unserer Plattform für politische Transparenz im
-        Mountainbike-Sport! Hier kannst du dich über die Haltungen und
-        Positionen der Politikerinnen und Politiker aus dem Kanton Zug
-        informieren und so herausfinden, wer die Interessen von uns
-        Mountainbikerinnen und Mountainbikern am besten vertritt.
+        Herzlich willkommen auf unserer Plattform zur Unterstützung einer
+        besseren Bikepolitik im Kanton Zug. Vor den National- und
+        Ständeratswahlen 2023 wollten wir Licht ins Dunkel bringen und die
+        Positionen der Kandidat*innen zum Thema Mountainbiking transparent
+        machen.
       </p>
       <p className="mb-4">
-        Die bevorstehenden Nationalratswahlen im Oktober 2023 bieten eine
-        hervorragende Gelegenheit, uns aktiv in den politischen Diskurs
-        einzubringen. Wir haben den Kandidatinnen und Kandidaten eine Reihe von
-        Fragen zu Themen rund ums Mountainbiken gestellt. Ihre Antworten geben
-        Aufschluss darüber, wie wichtig ihnen der Mountainbike-Sport und dessen
-        Förderung ist.
+        Unser Ziel: Dir und den etwa 22&#39;000 aktiven Mountainbikerinnen im
+        Kanton Zug, sowie allen weiteren interessierten Personen, eine
+        Entscheidungshilfe zu bieten. Welche Kandidatinnen könnten Deine
+        Interessen als Mountainbikerin am besten auf nationaler Ebene vertreten?
+        Mit dieser Plattform ermöglichen wir Dir, die Positionen der
+        Kandidatinnen zu vergleichen und so eine informierte Wahlentscheidung zu
+        treffen.
       </p>
       <p className="mb-4">
-        Auf dieser Webseite findest du die Antworten aller teilnehmenden
-        Politikerinnen und Politiker. Du kannst gezielt nach den Antworten
-        bestimmter Personen suchen, dir aber auch eine Übersicht über alle
-        Teilnehmenden verschaffen. Verschiedene Kennzahlen erleichtern dir zudem
-        den Überblick.
+        Die hier präsentierten Informationen basieren auf den Antworten, die wir
+        auf unseren Online-Fragebogen mit acht Fragen von den Kandidat*innen
+        erhalten haben. Die Teilnahme war freiwillig und die Ergebnisse wurden
+        Ende September 2023 veröffentlicht.
       </p>
       <p className="mb-4">
-        Unser Ziel ist es, dir dabei zu helfen, eine informierte Entscheidung
-        bei den kommenden Wahlen zu treffen. Nutze diese Informationen, um
-        diejenigen zu unterstützen, die unsere Leidenschaft für das
-        Mountainbiking teilen und bereit sind, sich für unsere Interessen
-        einzusetzen.
+        Wir glauben an den Mountainbike-Sport als einen etablierten und
+        zunehmend beliebteren Breitensport. Unser Ziel ist es, den
+        Mountainbike-Sport im Kanton Zug und in der gesamten Schweiz zu stärken
+        und zu fördern. Durch eine transparente und gut informierte
+        Wahlentscheidung kannst auch Du dazu beitragen.
       </p>
       <p>
-        Vielen Dank für deine Unterstützung und dein Engagement für den
-        Mountainbikesport. Jetzt ist die Zeit, unsere Stimme zu erheben und das
-        Mountainbiken in unserem Kanton weiter voranzutreiben!
+        Schau Dich auf unserer Plattform um, lerne die Positionen der
+        Kandidat*innen kennen und triff so die beste Wahlentscheidung. Danke für
+        Dein Interesse und Deine aktive Teilnahme.
       </p>
     </>
   );
