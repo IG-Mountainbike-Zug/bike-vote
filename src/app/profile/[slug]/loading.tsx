c7084ffc843e0f69f8fa2142526af9827beea019
Philipp Drebes <philipp.drebes@gmail.com>
2023-08-01 14:23:24 +0200

    add profile loading page

---
+++ src/app/profile/[slug]/loading.tsx
@@ -0,0 +1,67 @@
+export default function Page() {
+  return (
+    <>
+      <div className="animate-pulse">
+        <div className="px-4 sm:px-0">
+          <h3 className="text-3xl font-semibold text-gray-900">
+            <div className="h-8 w-4/5 rounded bg-gray-400"></div>
+          </h3>
+          <p className="text-md mt-1 max-w-2xl leading-6 text-gray-500">
+            <div className="h-4 w-2/5 rounded bg-gray-400"></div>
+          </p>
+          <div className="h-4 w-3/5 rounded bg-gray-400"></div>
+          <div className="h-4 w-4/5 rounded bg-gray-400"></div>
+          <div className="h-4 w-3/5 rounded bg-gray-400"></div>
+        </div>
+        <div className="mt-6 animate-pulse border-t border-gray-100 ">
+          <dl className="divide-y divide-gray-100">
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                <div className="h-4 w-4/5 rounded bg-gray-400"></div>
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                <div className="h-4 w-4/5 rounded bg-gray-400"></div>
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                <div className="h-4 w-4/5 rounded bg-gray-400"></div>
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                <div className="h-4 w-4/5 rounded bg-gray-400"></div>
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                <div className="h-4 w-4/5 rounded bg-gray-400"></div>
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                <div className="h-4 w-4/5 rounded bg-gray-400"></div>
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                <div className="h-4 w-4/5 rounded bg-gray-400"></div>
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                <div className="h-4 w-4/5 rounded bg-gray-400"></div>
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                <div className="h-4 w-3/5 rounded bg-gray-400"></div>
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                <div className="h-4 w-3/5 rounded bg-gray-400"></div>
+                <div className="h-4 w-4/5 rounded bg-gray-400"></div>
+                <div className="h-4 w-full rounded bg-gray-400"></div>
+                <div className="h-4 w-3/5 rounded bg-gray-400"></div>
+                <div className="h-4 w-4/5 rounded bg-gray-400"></div>
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </>
+  );
+}
