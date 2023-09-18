e5caf9a364f98e1d0962211ec323c4ad8714e11e
Philipp Drebes <philipp.drebes@gmail.com>
2023-09-18 21:16:38 +0200

    layouting

--- src/app/profile/[slug]/page.tsx
+++ src/app/profile/[slug]/page.tsx
@@ -92,10 +92,10 @@ export default async function page({ params }: { params: { slug: string } }) {
                 key={item.questionId}
                 className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0"
               >
-                <dt className="text-sm font-medium leading-6 text-gray-900 sm:col-span-2">
+                <dt className="text-sm  leading-6 text-gray-700 sm:col-span-2">
                   {item.questionDisplay}
                 </dt>
-                <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-0">
+                <dd className="mt-1 text-sm font-medium leading-6 text-gray-900 sm:mt-0">
                   {item.answerDisplay}
                 </dd>
               </div>
